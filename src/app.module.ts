3bc72e6df695c10c2c8ce441b1f5ea74981e2a6a
Jules Sorensen <julessorens@gmail.com>
2025-01-17 15:24:45 +0100

    tests: setup auth

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,6 +6,7 @@ import { AuthModule } from './auth/auth.module';
 import { BooksModule } from './books/books.module';
 import { User } from './users/entities/user.entity';
 import { UsersModule } from './users/users.module';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
   imports: [
--- src/auth/auth.service.spec.ts
+++ src/auth/auth.service.spec.ts
@@ -1,18 +1,48 @@
+import { JwtService } from '@nestjs/jwt';
 import { Test, TestingModule } from '@nestjs/testing';
+import * as bcrypt from 'bcrypt';
+import { UsersService } from '../users/users.service';
 import { AuthService } from './auth.service';
 
 describe('AuthService', () => {
   let service: AuthService;
+  let usersService: UsersService;
+  let jwtService: JwtService;
+
+  const mockUsersService = {
+    findOneByEmail: jest.fn(),
+  };
+
+  const mockJwtService = {
+    sign: jest.fn(),
+  };
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [AuthService],
+      providers: [
+        AuthService,
+        { provide: UsersService, useValue: mockUsersService },
+        { provide: JwtService, useValue: mockJwtService },
+      ],
     }).compile();
 
     service = module.get<AuthService>(AuthService);
+    usersService = module.get<UsersService>(UsersService);
+    jwtService = module.get<JwtService>(JwtService);
   });
 
   it('should be defined', () => {
     expect(service).toBeDefined();
+  });
+
+  describe('login', () => {
+    it('should return an access token', async () => {
+      const user = { id: 1, email: 'jules@zide.fr' };
+      const token = 'token';
+      mockJwtService.sign.mockReturnValue(token);
+
+      const result = await service.login(user);
+      expect(result).toEqual({ access_token: token });
+    });
   });
 });
