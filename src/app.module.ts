7fc25fc39572263316f515fd4003e4e87a532127
Yan <souetre.yan@gmail.com>
2025-01-17 11:23:41 +0100

    Create books and auth services and modules

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,13 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { AuthModule } from './auth/auth.module';
+import { BooksModule } from './books/books.module';
 import { DatabaseModule } from './database.module';
 
 @Module({
-  imports: [DatabaseModule],
+  imports: [AuthModule, BooksModule, DatabaseModule],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule { }
